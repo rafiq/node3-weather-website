58a6dc4f4f0fe094693ade804c3b8fad58566be0
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2022-01-11 10:45:47 +0300

    Set up app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,6 @@ const search = document.querySelector("input");
 const messageOne  = document.querySelector("#message-1")
 const messageTwo  = document.querySelector("#message-2")
 
-
 weatherForm.addEventListener("submit",(e) => {
     e.preventDefault()
     const location = search.value
@@ -11,7 +10,7 @@ weatherForm.addEventListener("submit",(e) => {
     messageOne.textContent = "Loading..."
     messageTwo.textContent = "";
 
-    fetch(`http://localhost:3000/weather?address=${location}`).then((res) => {
+    fetch(`/weather?address=${location}`).then((res) => {
         res.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = "Captain. There is a problem.";
